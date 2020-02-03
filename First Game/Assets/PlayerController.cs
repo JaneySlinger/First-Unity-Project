521faf37ee0edc6d2d3ba0baf4c3feceef399c41
Janey Slinger <janeyslinger@gmail.com>
2020-02-03 17:14:49 +0000

    Added basic movement

---
+++ First Game/Assets/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalMovement = Input.GetAxis("Horizontal");
+        float verticalMovement = Input.GetAxis("Vertical");
+        Rigidbody2D r = GetComponent<Rigidbody2D>() as Rigidbody2D;
+
+        r.velocity = new Vector2(horizontalMovement * speed, verticalMovement * speed);
+
+    }
+}
